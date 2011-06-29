494a7a2a69b860d688fe27e4b916bca6074558b5
Davide Bertola <dade@dadeb.it>
2011-06-29 20:21:25 +0200

    Database name passed as arg to patchDb

--- www/sqlite.js
+++ www/sqlite.js
@@ -16,18 +16,22 @@ function executeSQLite (path, query, success, fail) {
         GetFunctionName(fail), options);
 }
 
-function patchDb (dbPath) {
+function patchDb (dbPath, dbName) {
+    /*
+     * dbPath is the path to the Database.db index file webkitsqlite uses to store dbs configurations
+     * dbName is the name of the webkitsql database you want to patch
+     */
     openSQLite (dbPath, console.log, console.log);
     var query = "update origins set quota = '999999999999' where origin = 'file__0';";
     executeSQLite(dbPath, query, console.log, console.log);
-    var query2 = "update databases set estimatedSize = '999999999999' where name = 'database_patched';";
+    var query2 = "update databases set estimatedSize = '999999999999' where name = '" + dbName + "';";
     executeSQLite(dbPath, query2, console.log, console.log);
     closeSQLite (dbPath, console.log, console.log);
 }
 
 function onSuccess(fileSystem) {
     var dbPath = fileSystem.root.fullPath + "/../Library/WebKit/Databases/Databases.db";
-    patchDb (dbPath);
+    patchDb (dbPath, 'database_patched');
 }
 
 function startPatchingDb () {
