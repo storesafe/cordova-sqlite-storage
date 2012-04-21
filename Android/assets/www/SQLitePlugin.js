1f628821b1a537ca2530f404ed96358beb2adaf4
Chris Brody <chris.brody@gmail.com>
2012-04-21 19:14:23 +0200

    Cleanup comments for this.optimization_no_nested_callbacks (Android version)

--- Android/assets/www/SQLitePlugin.js
+++ Android/assets/www/SQLitePlugin.js
@@ -75,7 +75,10 @@
 		this.trans_id = get_unique_id();
 		this.__completed = false;
 		this.__submitted = false;
-		this.optimization_no_nested_callbacks = true;//if set to true large batches of queries within a transaction will be much faster but you _MAY_ lose the ability to do deep multi level nesting of executeSQL callbacks
+		//this.optimization_no_nested_callbacks: default is true.
+		//if set to true large batches of queries within a transaction will be much faster but 
+		//you will lose the ability to do multi level nesting of executeSQL callbacks
+		this.optimization_no_nested_callbacks = true;
 		console.log("root.SQLitePluginTransaction - this.trans_id:"+this.trans_id);
 		transaction_queue[this.trans_id] = [];
 		transaction_callback_queue[this.trans_id] = new Object();
