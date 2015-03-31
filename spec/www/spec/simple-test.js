84c581ea75add43bfa5220bc69bcd72fdf516578
Chris Brody <chris.brody@gmail.com>
2015-04-01 01:54:36 +0200

    Simple string test working for all target platforms; transaction & number binding tests working for iOS

--- spec/www/spec/simple-test.js
+++ spec/www/spec/simple-test.js
@@ -2,18 +2,20 @@ var MYTIMEOUT = 4000;
 
 var DEFAULT_SIZE = 5000000; // max to avoid popup in safari/ios
 
-var wait = 0;
-function stop() { ++wait; }
-
-function start() { --wait; if (wait == 0) done; }
-
+// XXX TODO replace in test(s):
 function ok(test, desc) { expect(test).toBe(true); }
 
-function equal(a, b, desc) { expect(a).toBe(b); }
+function equal(a, b, desc) { expect(a).toEqual(b); } // '=='
+
+function strictEqual(a, b, desc) { expect(a).toBe(b); } // '==='
+
+var isMSIE = /MSIE/.test(navigator.userAgent);
+var isIE = /Windows/.test(navigator.userAgent) || isMSIE;
+var isWebKit = !isIE; // TBD
 
 var scenarioList = [ 'Plugin', 'HTML5' ];
 
-var scenarioCount = /Windows/.test(navigator.userAgent) ? 1 : 2;
+var scenarioCount = isWebKit ? 2 : 1;
 
 describe('check startup', function() {
 
@@ -23,65 +25,66 @@ describe('check startup', function() {
   }, MYTIMEOUT);
 
   it('has openDatabase', function() {
-    expect(true).toBe(true);
-    expect(window.openDatabase).toBeDefined();
+    if (isWebKit) expect(window.openDatabase).toBeDefined();
     expect(window.sqlitePlugin).toBeDefined();
     expect(window.sqlitePlugin.openDatabase).toBeDefined();
-    //openDatabase = window.openDatabase = window.sqlitePlugin.openDatabase;
   });
 });
 
-for (var i=0; i<scenarioCount; ++i) {
-  var scenarioName = scenarioList[i];
-  var suiteName = scenarioName + ': ';
-  var isWebSql = (i !== 0);
+describe('simple tests', function() {
+  for (var i=0; i<scenarioCount; ++i) {
+    var scenarioName = scenarioList[i];
+    var suiteName = scenarioName + ': ';
+    var isWebSql = (i !== 0);
 
-  var openDatabase = isWebSql ? window.openDatabase : window.sqlitePlugin.openDatabase;
+    // NOTE: MUST be defined in function scope, NOT outer scope:
+    var openDatabase = function(name, ignored1, ignored2, ignored3) {
+      if (isWebSql) {
+        return window.openDatabase(name, "1.0", "Demo", DEFAULT_SIZE);
+      } else {
+        return window.sqlitePlugin.openDatabase(name, "1.0", "Demo", DEFAULT_SIZE);
+      }
+    }
 
-  describe('simple tests - ' + suiteName,
-    function() {
+    describe(suiteName + 'simple test(s)', function() {
 
-      it(suiteName + "US-ASCII String manipulation test",
-        function() {
+      describe(suiteName + 'simple string test(s)', function() {
+        it(suiteName + "US-ASCII String manipulation test", function(done) {
 
           var db = openDatabase("ASCII-string-test.db", "1.0", "Demo", DEFAULT_SIZE);
 
-          ok(!!db, 'valid db object');
-
-          stop(2);
+          expect(db).toBeDefined()
 
           db.transaction(function(tx) {
 
-            start(1);
-            ok(!!tx, 'valid tx object');
+            expect(tx).toBeDefined()
 
             tx.executeSql("select upper('Some US-ASCII text') as uppertext", [], function(tx, res) {
-              start(1);
-
               console.log("res.rows.item(0).uppertext: " + res.rows.item(0).uppertext);
+              expect(res.rows.item(0).uppertext).toEqual("SOME US-ASCII TEXT");
 
-              equal(res.rows.item(0).uppertext, "SOME US-ASCII TEXT", "select upper('Some US-ASCII text')");
+              done();
             });
           });
         }, MYTIMEOUT);
+      });
 
-        it(suiteName + "db transaction test", function() {
+      describe(suiteName + 'simple transaction test(s)', function() {
+        xit(suiteName + "db transaction test", function(done) {
           var db = openDatabase("db-trx-test.db", "1.0", "Demo", DEFAULT_SIZE);
 
           ok(!!db, "db object");
 
-          stop(10);
+          var check = 0;
 
           db.transaction(function(tx) {
 
-            start(1);
             ok(!!tx, "tx object");
 
             tx.executeSql('DROP TABLE IF EXISTS test_table');
             tx.executeSql('CREATE TABLE IF NOT EXISTS test_table (id integer primary key, data text, data_num integer)');
 
             tx.executeSql("INSERT INTO test_table (data, data_num) VALUES (?,?)", ["test", 100], function(tx, res) {
-              start(1);
               ok(!!tx, "tx object");
               ok(!!res, "res object");
 
@@ -92,11 +95,10 @@ for (var i=0; i<scenarioCount; ++i) {
               equal(res.rowsAffected, 1, "res rows affected");
 
               db.transaction(function(tx) {
-                start(1);
                 ok(!!tx, "second tx object");
 
                 tx.executeSql("SELECT count(id) as cnt from test_table;", [], function(tx, res) {
-                  start(1);
+                  ++check;
 
                   console.log("res.rows.length: " + res.rows.length + " -- should be 1");
                   console.log("res.rows.item(0).cnt: " + res.rows.item(0).cnt + " -- should be 1");
@@ -106,14 +108,14 @@ for (var i=0; i<scenarioCount; ++i) {
                 });
 
                 tx.executeSql("SELECT data_num from test_table;", [], function(tx, res) {
-                  start(1);
+                  ++check;
 
                   equal(res.rows.length, 1, "SELECT res rows length");
                   equal(res.rows.item(0).data_num, 100, "SELECT data_num");
                 });
 
                 tx.executeSql("UPDATE test_table SET data_num = ? WHERE data_num = 100", [101], function(tx, res) {
-                  start(1);
+                  ++check;
 
                   console.log("UPDATE rowsAffected: " + res.rowsAffected + " -- should be 1");
 
@@ -121,14 +123,14 @@ for (var i=0; i<scenarioCount; ++i) {
                 });
 
                 tx.executeSql("SELECT data_num from test_table;", [], function(tx, res) {
-                  start(1);
+                  ++check;
 
                   equal(res.rows.length, 1, "SELECT res rows length");
                   equal(res.rows.item(0).data_num, 101, "SELECT data_num");
                 });
 
                 tx.executeSql("DELETE FROM test_table WHERE data LIKE 'tes%'", [], function(tx, res) {
-                  start(1);
+                  ++check;
 
                   console.log("DELETE rowsAffected: " + res.rowsAffected + " -- should be 1");
 
@@ -136,27 +138,62 @@ for (var i=0; i<scenarioCount; ++i) {
                 });
 
                 tx.executeSql("SELECT data_num from test_table;", [], function(tx, res) {
-                  start(1);
+                  ++check;
 
                   equal(res.rows.length, 0, "SELECT res rows length");
                 });
 
+              }, function(e) {
+                console.log("ERROR: " + e.message);
+                expect(false);
+              }, function() {
+                console.log("second tx ok success cb");
+                expect(check).toBe(6);
+
+                done();
               });
 
             }, function(e) {
               console.log("ERROR: " + e.message);
+              expect(false);
             });
           }, function(e) {
             console.log("ERROR: " + e.message);
+            expect(false);
           }, function() {
             console.log("tx success cb");
-            ok(true, "tx success cb");
-            start(1);
           });
 
         }, MYTIMEOUT);
+      });
 
-  });
+      describe(suiteName + 'simple number binding test(s)', function() {
+        xit(suiteName + "number values inserted using number bindings", function(done) {
+          var db = openDatabase("Value-binding-test.db", "1.0", "Demo", DEFAULT_SIZE);
+          db.transaction(function(tx) {
+            tx.executeSql('DROP TABLE IF EXISTS test_table');
+            tx.executeSql('CREATE TABLE IF NOT EXISTS test_table (id integer primary key, data_text1, data_text2, data_int, data_real)');
+          }, function(err) { ok(false, err.message) }, function() {
+            db.transaction(function(tx) {
+              // create columns with no type affinity
+              tx.executeSql("insert into test_table (data_text1, data_text2, data_int, data_real) VALUES (?,?,?,?)", ["314159", "3.14159", 314159, 3.14159], function(tx, res) {
+                equal(res.rowsAffected, 1, "row inserted");
+                tx.executeSql("select * from test_table", [], function(tx, res) {
+                  var row = res.rows.item(0);
+                  strictEqual(row.data_text1, "314159", "data_text1 should have inserted data as text");
+                  strictEqual(row.data_text2, "3.14159", "data_text2 should have inserted data as text");
+                  strictEqual(row.data_int, 314159, "data_int should have inserted data as an integer");
+                  ok(Math.abs(row.data_real - 3.14159) < 0.000001, "data_real should have inserted data as a real");
 
-};
+                  done();
+                });
+              });
+            });
+          });
+        }, MYTIMEOUT);
+      });
+
+    });
+  };
+});
 
