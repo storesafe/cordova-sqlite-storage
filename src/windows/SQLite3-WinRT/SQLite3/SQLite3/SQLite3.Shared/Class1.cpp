d7afd6aff29c93cd62ebef1e9513e4f6f3c22350
Chris Brody <brodybits@litehelpers.net>
2015-04-15 16:16:21 +0200

    Generated Visual C++ Windows Universal Runtime Component project for SQLite3 library
    The following generated files and paths were omitted:
    ipch
    SQLite3.sdf
    SQLite3.v12.suo
    SQLite3/SQLite3.Windows/Debug (with binary cache file and extra log files)
    SQLite3/SQLite3.WindowsPhone/Debug (with binary cache file and extra log files)

---
+++ src/windows/SQLite3-WinRT/SQLite3/SQLite3/SQLite3.Shared/Class1.cpp
@@ -0,0 +1,9 @@
+﻿#include "pch.h"
+#include "Class1.h"
+
+using namespace SQLite3;
+using namespace Platform;
+
+Class1::Class1()
+{
+}
---
+++ src/windows/SQLite3-WinRT/SQLite3/SQLite3/SQLite3.Shared/Class1.h
@@ -0,0 +1,10 @@
+﻿#pragma once
+
+namespace SQLite3
+{
+	public ref class Class1 sealed
+	{
+	public:
+		Class1();
+	};
+}
