de45973439f0f1dbac61bb2f6e92affeb82c3671
Christopher J. Brody <brodybits@litehelpers.net>
2016-04-14 11:38:20 +0200

    Fix REGEXP test & add to Circle CI in this version branch

--- spec/www/spec/regexp-test.js
+++ spec/www/spec/regexp-test.js
@@ -4,12 +4,15 @@ var MYTIMEOUT = 12000;
 
 var DEFAULT_SIZE = 5000000; // max to avoid popup in safari/ios
 
-var isAndroid = /Android/.test(navigator.userAgent);
-var isWindows = /Windows /.test(navigator.userAgent); // Windows (8.1)
+// Detect actual platform:
+var isWP8 = /IEMobile/.test(navigator.userAgent); // Matches WP(7/8/8.1)
+var isWindows = /Windows /.test(navigator.userAgent); // Windows
+var isAndroidUA = /Android/.test(navigator.userAgent);
+var isAndroid = (isAndroidUA && !isWindows);
 
-var scenarioList = [ isAndroid ? 'Plugin-sqlite-connector' : 'Plugin', 'HTML5', 'Plugin-android.database' ];
+var scenarioList = [ isAndroid ? 'Plugin-implementation-default' : 'Plugin', 'HTML5', 'Plugin-implementation-2' ];
 
-var scenarioCount = isAndroid ? 3 : (isIE ? 1 : 2);
+var scenarioCount = (!!window.hasWebKitBrowser) ? (isAndroid ? 3 : 2) : 1;
 
 // simple tests:
 var mytests = function() {
@@ -20,25 +23,29 @@ var mytests = function() {
       var scenarioName = scenarioList[i];
       var suiteName = scenarioName + ': ';
       var isWebSql = (i === 1);
-      var isOldImpl = (i === 2);
+      var isImplementation2 = (i === 2);
+      var isAndroidSQLiteConnector = false; // (NOT in this version branch)
 
       // NOTE: MUST be defined in function scope, NOT outer scope:
       var openDatabase = function(name, ignored1, ignored2, ignored3) {
-        if (isOldImpl) {
-          return window.sqlitePlugin.openDatabase({name: name, androidDatabaseImplementation: 2});
+        if (isImplementation2) {
+          return window.sqlitePlugin.openDatabase({name: name, location: 1, androidDatabaseImplementation: 2});
         }
         if (isWebSql) {
           return window.openDatabase(name, "1.0", "Demo", DEFAULT_SIZE);
         } else {
-          return window.sqlitePlugin.openDatabase(name, "1.0", "Demo", DEFAULT_SIZE);
+          return window.sqlitePlugin.openDatabase({name: name, location: 0});
         }
       }
 
       it(suiteName + 'Simple REGEXP test',
         function(done) {
-          if (isWebSql && !isAndroid) pending('BROKEN for iOS Web SQL');
-          if (!isWebSql && isAndroid && !isOldImpl) pending('BROKEN for default Android implementation');
-          if (isWindows) pending('BROKEN for Windows ("Universal")');
+          // Test for Android Web SQL ONLY
+          if (isWP8) pending('NOT IMPLEMENTED for WP8');
+          if (isWindows) pending('NOT IMPLEMENTED for Windows');
+          //if (!isWebSql && isAndroid && isImplementation2 && /Android [1-4]/.test(navigator.userAgent)) pending('BROKEN for android.database (version 1.x-4.x)');
+          if (!isWebSql && isAndroid) pending('SKIP for android.database'); // TBD (SKIP for Android plugin for now)
+          if (!isAndroid && !isWindows && !isWP8) pending('SKIP for iOS');
 
           var db = openDatabase('simple-regexp-test.db', '1.0', 'test', DEFAULT_SIZE);
 
