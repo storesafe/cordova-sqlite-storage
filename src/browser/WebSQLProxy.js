c252c47919e9399a2691e8a527995a636aa44f29
raf_w87@wp.pl <raf_w87@wp.pl>
2016-10-24 21:55:06 +0200

    Browser platform - added fake support for .deleteDatabase() by deleting all tables

--- src/browser/WebSQLProxy.js
+++ src/browser/WebSQLProxy.js
@@ -33,7 +33,7 @@ function openFn(success, error, args) {
         log.verbose = openargs.verbose == true;
         log.debug("SQLitePlugin.open", args);
         var dbName = openargs.name;
-        var db = openDatabase(dbName, "", dbName, 5 * 1024 * 2014);
+        var db = openDatabase(dbName, "", dbName, 5 * 1024 * 1024);
         databases[dbName] = db;
         success();
     } catch(err) {
@@ -54,12 +54,95 @@ function closeFn(success, error, args) {
 
 function deleteFn(success, error, args) {
     success = success || function(){};
+    error = error || function(){};
 
     log.debug("SQLitePlugin.delete", args);
-    log.warn("SQLitePlugin.delete is not supported in browser");
-    var dbName = args[0].name;
-    delete databases[dbName];
-    success();
+    var dbName = args[0].path;
+
+    var db = databases[dbName];
+    if(!db) {
+        success();
+        return;
+    }
+    // these tables should not be dropped; to be extended if needed (regexp or string)
+    var specialTables = [/^__.+__$/, /^sqlite_/];
+
+    function isSpecialTable(tableName) {
+        for(var i=0; i<specialTables.length; i++) {
+            if(typeof specialTables[i].test == "function" && specialTables[i].test(tableName)) {
+                return true;
+            }
+            if(typeof specialTables[i] == "string" && specialTables[i] == tableName) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    var dropStatus = {};
+    function fireCallbackIfCompleted() {
+        var completed = true;
+        var errors = [];
+        for(var tableName in dropStatus) {
+            if(dropStatus[tableName] == null) {
+                completed = false;
+                break;
+            }
+            if(dropStatus[tableName] == false) {
+                errors.push(tableName);
+            }
+        }
+        if(completed) {
+            if(errors.length > 0) {
+                error(new Error("Some tables were not dropped: " + errors.join(', ')));
+            } else {
+                delete databases[dbName];
+                success();
+            }
+        }
+    }
+
+
+    function dropTable(tx, tableName) {
+        tx.executeSql("DROP TABLE IF EXISTS " + tableName, [], function(){
+            log.debug("Table '" + tableName + "' has been dropped.");
+            dropStatus[tableName] = true;
+            fireCallbackIfCompleted();
+        }, function(err) {
+            log.error(err);
+            dropStatus[tableName] = false;
+            fireCallbackIfCompleted();
+        });
+    }
+
+    function dropSelectedTables(tx, results) {
+        var tablesToDrop = [];
+        for(var i=0; i<results.rows.length; i++) {
+            var tableName = results.rows.item(i).tbl_name;
+            if(!isSpecialTable(tableName)) {
+                tablesToDrop.push(tableName);
+                dropStatus[tableName] = null;
+            }
+        }
+        if(tablesToDrop.length == 0) {
+            delete databases[dbName];
+            success();
+        } else {
+            for(var i=0; i<tablesToDrop.length; i++) {
+                dropTable(tx, tablesToDrop[i]);
+            }
+        }
+    }
+
+    function dropTables(tx) {
+        tx.executeSql("SELECT tbl_name from sqlite_master WHERE type='table'", [], dropSelectedTables, function(err) {
+            log.error(err);
+            error(err);
+        });
+    }
+
+    db.transaction(dropTables);
 }
 
 function executeSqlBatchFn(success, error, args) {
