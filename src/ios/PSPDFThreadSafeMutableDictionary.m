570d5e8a793f7441bb7996d809abd884569dc729
Christopher J. Brody <brodybits@litehelpers.net>
2017-09-05 19:36:04 -0400

    Fix PSPDFThreadSafeMutableDictionary.m redeclaration issue

--- src/ios/PSPDFThreadSafeMutableDictionary.m
+++ src/ios/PSPDFThreadSafeMutableDictionary.m
@@ -23,9 +23,11 @@
 
 #import <Foundation/Foundation.h>
 
+/* ** ALREADY INCLUDED BY #import "PSPDFThreadSafeMutableDictionary.h"
 // Dictionary-Subclasss whose primitive operations are thread safe.
 @interface PSPDFThreadSafeMutableDictionary : NSMutableDictionary
 @end
+// */
 
 // ----------------------------------------------------------------
 
