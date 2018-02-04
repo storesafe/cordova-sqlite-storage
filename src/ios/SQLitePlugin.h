feb7045620ea095408bfe20482e7c6628f44ad66
Christopher J. Brody <brodybits@litehelpers.net>
2018-02-04 14:21:02 -0500

    iOS cleanup remove internal functions from header

--- src/ios/SQLitePlugin.h
+++ src/ios/SQLitePlugin.h
@@ -36,13 +36,7 @@ typedef int WebSQLError;
 -(void) close: (CDVInvokedUrlCommand*)command;
 -(void) delete: (CDVInvokedUrlCommand*)command;
 
--(void) openNow: (CDVInvokedUrlCommand*)command;
--(void) closeNow: (CDVInvokedUrlCommand*)command;
--(void) deleteNow: (CDVInvokedUrlCommand*)command;
-
 // Batch processing interface
 -(void) backgroundExecuteSqlBatch: (CDVInvokedUrlCommand*)command;
-
--(void) executeSqlBatchNow: (CDVInvokedUrlCommand*)command;
 
 @end /* vim: set expandtab : */
