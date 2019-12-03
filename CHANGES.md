ac9a8eaee16dde3b809736360be009f4fdef9901
Christopher J. Brody <chris@brody.consulting>
2019-12-02 23:39:23 -0500

    cordova-sqlite-storage-dependencies@2.1.1 update
    with SQLite 3.30.1

--- CHANGES.md
+++ CHANGES.md
@@ -2,7 +2,7 @@
 
 #### cordova-sqlite-storage 3.4.1-dev
 
-TBD
+- SQLite 3.31.1 update from cordova-sqlite-storage-dependencies@2.1.1
 
 #### cordova-sqlite-storage 3.4.0
 
--- spec/www/spec/sqlite-version-test.js
+++ spec/www/spec/sqlite-version-test.js
@@ -104,7 +104,7 @@ var mytests = function() {
               expect(rs).toBeDefined();
               expect(rs.rows).toBeDefined();
               expect(rs.rows.length).toBe(1);
-              expect(rs.rows.item(0).myResult).toBe('3.28.0');
+              expect(rs.rows.item(0).myResult).toBe('3.30.1');
 
               // Close (plugin only) & finish:
               (isWebSql) ? done() : db.close(done, done);
