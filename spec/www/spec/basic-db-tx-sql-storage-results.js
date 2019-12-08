8cf0ba6fdf4b0e3fbaff8301421c123568abec75
Christopher J. Brody <chris@brody.consulting>
2019-12-08 18:56:39 -0500

    [TBD] add PENDING rename table with view test
    PENDING due to KNOWN CRASH on SQLite 3.30.1 (Decmeber 2019) ref:
    
    - http://sqlite.1065341.n5.nabble.com/Crash-Bug-Report-tc109903.html
    - https://github.com/xpbrew/cordova-sqlite-storage/issues/904

--- spec/www/spec/basic-db-tx-sql-storage-results.js
+++ spec/www/spec/basic-db-tx-sql-storage-results.js
@@ -1079,6 +1079,45 @@ var mytests = function() {
           }
         }, MYTIMEOUT);
 
+        // TBD December 2019 SQLite crash report ref:
+        // - http://sqlite.1065341.n5.nabble.com/Crash-Bug-Report-tc109903.html
+        // - https://github.com/xpbrew/cordova-sqlite-storage/issues/904
+        xit(suiteName + 'RENAME table with view test [SQLite crash bug report December 2019]', function(done) {
+          if (isWebSql) pending('[TBD] NOT WORKING on (WebKit) Web SQL');
+          if (!isWebSql && isAndroid && isImpl2) pending('[TBD] NOT WORKING on Android system android.database provider');
+
+          var dbname = 'RENAME-table-with-view-test.db';
+          var db = openDatabase(dbname);
+
+          db.transaction(function(tx) {
+            tx.executeSql('DROP TABLE IF EXISTS v0;');
+            tx.executeSql('DROP TABLE IF EXISTS v2;');
+            tx.executeSql('DROP TABLE IF EXISTS t3;');
+            tx.executeSql('DROP VIEW IF EXISTS v4;');
+            tx.executeSql('DROP TRIGGER IF EXISTS x;');
+            tx.executeSql('CREATE TABLE v0 ( v1 ) ;');
+            tx.executeSql('CREATE TABLE v2 ( v3 ) ;');
+            tx.executeSql('CREATE VIEW v4 AS WITH x AS ( SELECT x () OVER( ) FROM v4 ) SELECT v3 AS x FROM v2 ;');
+            tx.executeSql('ALTER TABLE v2 RENAME TO t3;');
+            tx.executeSql('SELECT COUNT(x) AS cnt FROM v4;', null, function(tx, rs) {
+              expect(rs).toBeDefined();
+              expect(rs.rows.length).toBe(1);
+              expect(rs.rows.item(0).cnt).toBe(0);
+
+              // Close (plugin only) & finish:
+              (isWebSql) ? done() : db.close(done, done);
+            });
+          }, function(e) {
+            // ERROR RESULT (NOT EXPECTED):
+            expect(false).toBe(true);
+            expect(e).toBeDefined();
+            expect(e.message).toBe('--');
+
+            // Close (plugin only) & finish:
+            (isWebSql) ? done() : db.close(done, done);
+          });
+        }, MYTIMEOUT);
+
       });
 
     });
